fdee9ad6c959ae01ab998b324390e2e976fd8b10
Arneo Santos <awpdoss50@gmail.com>
2024-06-08 17:53:32 +0100

    Pequena alteração

--- ProjetoDA/Views/Prato.Designer.cs
+++ ProjetoDA/Views/Prato.Designer.cs
@@ -44,53 +44,59 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(268, 9);
+            this.label1.Location = new System.Drawing.Point(201, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(265, 32);
+            this.label1.Size = new System.Drawing.Size(209, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Gestão dos Pratos";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(274, 68);
+            this.dataGridView1.Location = new System.Drawing.Point(206, 55);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(514, 370);
+            this.dataGridView1.Size = new System.Drawing.Size(386, 301);
             this.dataGridView1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(182, 141);
+            this.button1.Location = new System.Drawing.Point(136, 115);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(77, 29);
+            this.button1.Size = new System.Drawing.Size(60, 24);
             this.button1.TabIndex = 2;
             this.button1.Text = "Adicionar";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(109, 68);
+            this.textBox1.Location = new System.Drawing.Point(82, 55);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(150, 22);
+            this.textBox1.Size = new System.Drawing.Size(114, 20);
             this.textBox1.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 71);
+            this.label2.Location = new System.Drawing.Point(2, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.Size = new System.Drawing.Size(80, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Nome do prato:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(65, 104);
+            this.label3.Location = new System.Drawing.Point(49, 84);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 16);
+            this.label3.Size = new System.Drawing.Size(31, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Tipo:";
             // 
@@ -101,34 +107,37 @@
             "Carne",
             "Peixe",
             "Vegan"});
-            this.comboBox1.Location = new System.Drawing.Point(109, 101);
+            this.comboBox1.Location = new System.Drawing.Point(82, 82);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(150, 24);
+            this.comboBox1.Size = new System.Drawing.Size(114, 21);
             this.comboBox1.TabIndex = 6;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(109, 141);
+            this.button2.Location = new System.Drawing.Point(82, 115);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(67, 29);
+            this.button2.Size = new System.Drawing.Size(50, 24);
             this.button2.TabIndex = 7;
             this.button2.Text = "Editar";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // BtnApagar
             // 
-            this.BtnApagar.Location = new System.Drawing.Point(109, 176);
+            this.BtnApagar.Location = new System.Drawing.Point(82, 143);
+            this.BtnApagar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BtnApagar.Name = "BtnApagar";
-            this.BtnApagar.Size = new System.Drawing.Size(148, 25);
+            this.BtnApagar.Size = new System.Drawing.Size(114, 22);
             this.BtnApagar.TabIndex = 8;
             this.BtnApagar.Text = "Apagar";
             this.BtnApagar.UseVisualStyleBackColor = true;
             // 
             // Prato
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.BtnApagar);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.comboBox1);
@@ -138,6 +147,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Prato";
             this.Text = "Prato";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
