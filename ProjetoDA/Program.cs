0f3b547731ce3ef0a12fc3988588969f87be2682
Arneo Santos <awpdoss50@gmail.com>
2024-06-08 19:28:43 +0100

    Program Form1

--- ProjetoDA/Program.cs
+++ ProjetoDA/Program.cs
@@ -21,7 +21,7 @@ namespace ProjetoDA
             } 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Prato());
+            Application.Run(new Form1());
         }
     }
 }
