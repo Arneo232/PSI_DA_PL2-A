1899eea293d41abeefc72f242da94a84054f854d
Arneo Santos <83582189+Arneo232@users.noreply.github.com>
2024-06-15 19:17:45 +0100

    Upload

---
+++ Rapido/Rapido/Modelo/Prato.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rapido.Modelo
+{
+    internal class Prato
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TypePrato Tipo { get; set; }
+
+        public enum TypePrato
+        {
+            a, b, c
+        }
+    }
+}
