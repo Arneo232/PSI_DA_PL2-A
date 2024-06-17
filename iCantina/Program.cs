65b98f569074fac3cbc0fd56bff2545e2c2e912f
eduemleiria <151396101+eduemleiria@users.noreply.github.com>
2024-06-18 00:28:51 +0100

    Formulário pratos feito com as funcionalidades a funcionar.

--- iCantina/Program.cs
+++ iCantina/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using iCantina.Views;
 
 namespace iCantina
 {
@@ -16,7 +17,7 @@ namespace iCantina
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormPrato());
         }
     }
 }
