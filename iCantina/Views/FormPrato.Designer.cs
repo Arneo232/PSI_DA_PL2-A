62545c169840cbe1f4616df45a291ef4f22eafe4
eduemleiria <151396101+eduemleiria@users.noreply.github.com>
2024-06-17 18:38:11 +0100

    Formulário do prato feito.

--- iCantina/Views/FormPrato.Designer.cs
+++ iCantina/Views/FormPrato.Designer.cs
@@ -29,46 +29,240 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.dgvPratos = new System.Windows.Forms.DataGridView();
+            this.TSM = new System.Windows.Forms.MenuStrip();
+            this.TSM_GestaoC = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSM_GestaoP = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSM_GestaoM = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSM_GestaoF = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSM_GestaoMulta = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSM_Registo = new System.Windows.Forms.ToolStripMenuItem();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbTipoPrato = new System.Windows.Forms.ComboBox();
+            this.cbEstado = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPratos)).BeginInit();
+            this.TSM.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(105, 61);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 81);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(72, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Descrição:";
             // 
-            // textBox1
+            // txtDescricao
             // 
-            this.textBox1.Location = new System.Drawing.Point(204, 61);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtDescricao.Location = new System.Drawing.Point(99, 80);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(122, 20);
+            this.txtDescricao.TabIndex = 1;
             // 
-            // dataGridView1
+            // dgvPratos
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(433, 163);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 2;
+            this.dgvPratos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPratos.Location = new System.Drawing.Point(239, 80);
+            this.dgvPratos.Name = "dgvPratos";
+            this.dgvPratos.Size = new System.Drawing.Size(420, 358);
+            this.dgvPratos.TabIndex = 2;
+            // 
+            // TSM
+            // 
+            this.TSM.Dock = System.Windows.Forms.DockStyle.None;
+            this.TSM.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TSM.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSM_GestaoC,
+            this.TSM_GestaoP,
+            this.TSM_GestaoM,
+            this.TSM_GestaoF,
+            this.TSM_GestaoMulta,
+            this.TSM_Registo});
+            this.TSM.Location = new System.Drawing.Point(-1, 36);
+            this.TSM.Name = "TSM";
+            this.TSM.Size = new System.Drawing.Size(673, 24);
+            this.TSM.TabIndex = 62;
+            this.TSM.Text = "TSM Tabs";
+            // 
+            // TSM_GestaoC
+            // 
+            this.TSM_GestaoC.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TSM_GestaoC.Name = "TSM_GestaoC";
+            this.TSM_GestaoC.Size = new System.Drawing.Size(102, 20);
+            this.TSM_GestaoC.Text = "Gestâo do Cliente";
+            // 
+            // TSM_GestaoP
+            // 
+            this.TSM_GestaoP.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TSM_GestaoP.Name = "TSM_GestaoP";
+            this.TSM_GestaoP.Size = new System.Drawing.Size(105, 20);
+            this.TSM_GestaoP.Text = "Gestão dos Pratos";
+            // 
+            // TSM_GestaoM
+            // 
+            this.TSM_GestaoM.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TSM_GestaoM.Name = "TSM_GestaoM";
+            this.TSM_GestaoM.Size = new System.Drawing.Size(100, 20);
+            this.TSM_GestaoM.Text = " Gestão do Menu";
+            // 
+            // TSM_GestaoF
+            // 
+            this.TSM_GestaoF.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TSM_GestaoF.Name = "TSM_GestaoF";
+            this.TSM_GestaoF.Size = new System.Drawing.Size(106, 20);
+            this.TSM_GestaoF.Text = "Gestão de Faturas";
+            // 
+            // TSM_GestaoMulta
+            // 
+            this.TSM_GestaoMulta.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TSM_GestaoMulta.Name = "TSM_GestaoMulta";
+            this.TSM_GestaoMulta.Size = new System.Drawing.Size(100, 20);
+            this.TSM_GestaoMulta.Text = "Gestão de Multas";
+            // 
+            // TSM_Registo
+            // 
+            this.TSM_Registo.Name = "TSM_Registo";
+            this.TSM_Registo.Size = new System.Drawing.Size(152, 20);
+            this.TSM_Registo.Text = "Registo de Funcionário";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(234, 9);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 26);
+            this.label5.TabIndex = 61;
+            this.label5.Text = "Gestão de Pratos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 63;
+            this.label2.Text = "Tipo de prato:";
+            // 
+            // cbTipoPrato
+            // 
+            this.cbTipoPrato.FormattingEnabled = true;
+            this.cbTipoPrato.Location = new System.Drawing.Point(99, 115);
+            this.cbTipoPrato.Name = "cbTipoPrato";
+            this.cbTipoPrato.Size = new System.Drawing.Size(122, 21);
+            this.cbTipoPrato.TabIndex = 64;
+            // 
+            // cbEstado
+            // 
+            this.cbEstado.FormattingEnabled = true;
+            this.cbEstado.Location = new System.Drawing.Point(99, 151);
+            this.cbEstado.Name = "cbEstado";
+            this.cbEstado.Size = new System.Drawing.Size(122, 21);
+            this.cbEstado.TabIndex = 66;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 65;
+            this.label3.Text = "Estado:";
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnAdicionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicionar.Location = new System.Drawing.Point(99, 178);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(122, 26);
+            this.btnAdicionar.TabIndex = 67;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = false;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.Cyan;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(99, 206);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(70, 26);
+            this.btnGuardar.TabIndex = 68;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.Yellow;
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(166, 206);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(55, 26);
+            this.btnEditar.TabIndex = 69;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.BackColor = System.Drawing.Color.Red;
+            this.btnApagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagar.Location = new System.Drawing.Point(99, 234);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(122, 26);
+            this.btnApagar.TabIndex = 70;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::iCantina.Properties.Resources.Logo2;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 266);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 172);
+            this.pictureBox1.TabIndex = 71;
+            this.pictureBox1.TabStop = false;
             // 
             // FormPrato
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox1);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(671, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.cbEstado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbTipoPrato);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TSM);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dgvPratos);
+            this.Controls.Add(this.txtDescricao);
             this.Controls.Add(this.label1);
             this.Name = "FormPrato";
             this.Text = "FormPrato";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPratos)).EndInit();
+            this.TSM.ResumeLayout(false);
+            this.TSM.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,7 +271,24 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.DataGridView dgvPratos;
+        private System.Windows.Forms.MenuStrip TSM;
+        private System.Windows.Forms.ToolStripMenuItem TSM_GestaoC;
+        private System.Windows.Forms.ToolStripMenuItem TSM_GestaoP;
+        private System.Windows.Forms.ToolStripMenuItem TSM_GestaoM;
+        private System.Windows.Forms.ToolStripMenuItem TSM_GestaoF;
+        private System.Windows.Forms.ToolStripMenuItem TSM_GestaoMulta;
+        private System.Windows.Forms.ToolStripMenuItem TSM_Registo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbTipoPrato;
+        private System.Windows.Forms.ComboBox cbEstado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
